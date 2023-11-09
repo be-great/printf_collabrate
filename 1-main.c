d853e5de8378e6c963165192f2bbc00ff3e33be0
Isaiah woko <wokoisaiah68@gmail.com>
2023-11-09 13:34:43 +0100

    made changes in 1-main.c file

--- 1-main.c
+++ 1-main.c
@@ -5,9 +5,15 @@ int add(int a, int b)
 	return (a + b);
 }
 
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
 
 int main(void)
 {
-	printf("%d\n", add(10 + 100));
+	printf("%d\n", add(10, 100));
+	printf("%d\n", sub(100, 10));
 	return (0);
 }
